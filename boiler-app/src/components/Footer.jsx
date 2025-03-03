2baefbbebd69145659d2e4b8434a97dd6f568a3f
trosdesuru <eduardhernandezventos@gmail.com>
2025-03-03 19:11:55 +0100

    refactorized Header and Footer

--- boiler-app/src/components/Footer.jsx
+++ boiler-app/src/components/Footer.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
 function Footer() {
-    console.info('Header -> call')
+    console.info('Footer -> call')
 
     return (
-        <header className="fixed bottom-0 left-0 right-0 p-4 bg-purple-600 text-white">
+        <header className="fixed bottom-0 left-0 mt-6 right-0 p-4 bg-purple-600 text-white">
             <h1>BoilerPlate React App | Footer</h1>
         </header>
     )
--- boiler-app/src/components/Header.jsx
+++ boiler-app/src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
     console.info('Header -> call')
 
     return (
-        <header className="fixed top-0 left-0 right-0 p-4 bg-purple-600 text-white">
+        <header className="fixed top-0 right-0 left-0 p-4 bg-purple-600 text-white">
             <h1>BoilerPlate React App | Header</h1>
         </header>
     )
