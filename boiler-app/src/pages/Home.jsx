a0e2edab7062e86402f260b43534787fdda806a7
trosdesuru <eduardhernandezventos@gmail.com>
2025-03-03 13:46:36 +0100

    refactor: Update styles and fetch places in Home component

--- boiler-app/src/pages/Home.jsx
+++ boiler-app/src/pages/Home.jsx
@@ -1,32 +1,69 @@
 import React, { useEffect, useState } from 'react'
 
 function Home() {
-    console.info('Home -> call')
+    // Home -> call
 
-    const [posts, setPosts] = useState([])
+    const [places, setPlaces] = useState([])
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        console.info('fetch -> call')
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: 'fsq3uoWiTdo2zn+ysoIXryvZq2Ii7/c9qOBtA7TfrWkeEi4='
+            }
+        }
 
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then((response) => response.json())
-            .then((data) => setPosts(data.slice(0, 5)))
-            .catch((error) => console.error('error fetching posts:', error))
+        const queryParams = new URLSearchParams({
+            ll: '41.3874,2.1686',
+            radius: 1000,
+            limit: 10
+        })
+
+        fetch(`https://api.foursquare.com/v3/places/search?${queryParams}`, options)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`)
+                }
+                return response.json()
+            })
+            .then((data) => {
+                setPlaces(data.results)
+                setLoading(false)
+            })
+            .catch((error) => {
+                console.error('Error fetching places:', error)
+                setError(error)
+                setLoading(false)
+            })
     }, [])
 
     return (
         <div className="p-4">
-            <h2 className="text-xl font-bold">Posts</h2>
-            <ul>
-                {posts.map((post) => (
-                    <li key={post.id} className="border-b p-2">
-                        <h3 className="font-semibold">{post.title}</h3>
-                        <p>{post.body}</p>
-                    </li>
-                ))}
-            </ul>
+            <h1 className="text-left text-xl font-bold">Places</h1>
+            {loading ? (
+                <p class='text-left'>Loading...</p>
+            ) : error ? (
+                <p className="text-red-500">Error: {error}</p>
+            ) : (
+                <ul>
+                    {places.map((place) => (
+                        <li key={place.fsq_id} className="border-b p-2">
+                            <h2 className="text-left text-amber-300 font-semibold">{place.name}</h2>
+                            <p className='text-left'>{place.location.formatted_address}</p>
+                            {place.categories.length > 0 && (
+                                <p className="text-left text-sm text-zinc-500">
+                                    Categoría: {place.categories[0].name}
+                                </p>
+                            )}
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
-    )
+    );
 }
 
 export default Home
