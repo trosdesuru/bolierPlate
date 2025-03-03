06cb7e7fa0a90662fda6bac1d30cd8611084d70d
trosdesuru <eduardhernandezventos@gmail.com>
2025-03-03 19:12:29 +0100

    reafactorized Home.jsx

--- boiler-app/src/pages/Home.jsx
+++ boiler-app/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 
 function Home() {
-    // Home -> call
+    console.info('Home -> call')
 
     const [places, setPlaces] = useState([])
     const [error, setError] = useState(null)
@@ -12,14 +12,14 @@ function Home() {
             method: 'GET',
             headers: {
                 accept: 'application/json',
-                Authorization: 'fsq3uoWiTdo2zn+ysoIXryvZq2Ii7/c9qOBtA7TfrWkeEi4='
+                Authorization: process.env.REACT_APP_FOURSQUARE_API_KEY
             }
         }
 
         const queryParams = new URLSearchParams({
             ll: '41.3874,2.1686',
             radius: 1000,
-            limit: 10
+            limit: 20
         })
 
         fetch(`https://api.foursquare.com/v3/places/search?${queryParams}`, options)
@@ -42,7 +42,7 @@ function Home() {
 
     return (
         <div className="p-4">
-            <h1 className="text-left text-xl font-bold">Places</h1>
+            <h1 className="mt-6 text-left text-xl font-bold">Places</h1>
             {loading ? (
                 <p className='text-left'>Loading...</p>
             ) : error ? (
