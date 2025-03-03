30bc301fabdc9236a246a6d5fd8039443149ea4f
trosdesuru <eduardhernandezventos@gmail.com>
2025-03-03 13:45:24 +0100

    refactorize components/src

---
+++ boiler-app/api/test.sh
@@ -0,0 +1,4 @@
+curl --request GET \
+     --url https://api.foursquare.com/v3/places/search \
+     --header 'Authorization: fsq3uoWiTdo2zn+ysoIXryvZq2Ii7/c9qOBtA7TfrWkeEi4=' \
+     --header 'accept: application/json'
